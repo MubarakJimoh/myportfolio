08db6f059d996b17efe87edeb97d98e681aa2deb
MubarakJimoh <olajimohmj@gmail.com>
2022-10-19 02:57:39 +0000

    new commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import installNode from '../assets/portfolio/installNode.jpg'
-import navbar from '../assets/portfolio/navbar.jpg'
+import installNode from '../assets/portfolio/jmportfolio.png'
+import navbar from '../assets/portfolio/reactWeather.jpg'
 import reactParallax from '../assets/portfolio/reactParallax.jpg'
 import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
 
@@ -10,18 +10,27 @@ const Experience = () => {
         {
           id: 1,
           src: installNode,
+          href1: "https://github.com/mubarakjimoh",
+          href2: "https://jimohmportfolio.netlify.app",
+
         },
         {
           id: 2,
           src: navbar,
+          href1: "https://github.com/mubarakjimoh",
+          href2: "https://jimohmportfolio.netlify.app",
         },
         {
           id: 3,
           src: reactParallax,
+          href1: "https://github.com/mubarakjimoh",
+          href2: "https://jimohmportfolio.netlify.app",
         },
         {
           id: 4,
           src: reactSmooth,
+          href1: "https://github.com/mubarakjimoh",
+          href2: "https://jimohmportfolio.netlify.app",
         },
         
         
@@ -40,18 +49,26 @@ const Experience = () => {
             </div>
             <div className='grid sm:grid-cols-2 
             md:grid-cols-3 gap-8  px-12 sm:px-0'>
-            {links.map(({id, src}) => (
+            {links.map(({id, src, href1, href2}) => (
              <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+              
                 <img src= {src} alt=''
                 className='rounded-md duration-200
-                           hover:scale-105 ' 
+                           hover:scale-105  ' 
                            key={id}/>
+            
+                  
+              
         
                 <div className='flex items-center justify-center'>
+                    <button  className='w-1/2 px-6 py-3
+                    duratin-200 hover:scale-105'>
+                      <a href= {href1}>github</a>
+                    </button> 
                     <button className='w-1/2 px-6 py-3
-                    duratin-200 hover:scale-105'>Demo</button>
-                    <button className='w-1/2 px-6 py-3
-                    duratin-200 hover:scale-105'>Code</button>
+                    duratin-200 hover:scale-105'>
+                       <a href= {href2}>Demo</a>
+                    </button>
                 </div>
             </div>
             ))}
